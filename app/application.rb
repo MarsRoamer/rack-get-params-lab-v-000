8ccd2f5dc9526aa3d2c556224567829b567d660a
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-13 17:26:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -31,7 +31,7 @@ class Application
         @@cart << item
         resp.write "added #{item}"
       else
-        resp.write "Error"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
